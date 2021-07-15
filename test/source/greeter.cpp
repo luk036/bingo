75cc8eda7e5a6efdfa95ffb2f83a8384f299be14
Live session user <luk036@gmail.com>
2021-07-15 23:51:18 +0800

    change the format

--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -1,21 +1,21 @@
-#include <doctest/doctest.h>
 #include <bingo/greeter.h>
 #include <bingo/version.h>
+#include <doctest/doctest.h>
 
 #include <string>
 
 TEST_CASE("Bingo") {
-  using namespace bingo;
+    using namespace bingo;
 
-  Bingo bingo("Tests");
+    Bingo bingo("Tests");
 
-  CHECK(bingo.greet(LanguageCode::EN) == "Hello, Tests!");
-  CHECK(bingo.greet(LanguageCode::DE) == "Hallo Tests!");
-  CHECK(bingo.greet(LanguageCode::ES) == "¡Hola Tests!");
-  CHECK(bingo.greet(LanguageCode::FR) == "Bonjour Tests!");
+    CHECK(bingo.greet(LanguageCode::EN) == "Hello, Tests!");
+    CHECK(bingo.greet(LanguageCode::DE) == "Hallo Tests!");
+    CHECK(bingo.greet(LanguageCode::ES) == "¡Hola Tests!");
+    CHECK(bingo.greet(LanguageCode::FR) == "Bonjour Tests!");
 }
 
 TEST_CASE("Bingo version") {
-  static_assert(std::string_view(BINGO_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(BINGO_VERSION) == std::string("1.0"));
+    static_assert(std::string_view(BINGO_VERSION) == std::string_view("1.0"));
+    CHECK(std::string(BINGO_VERSION) == std::string("1.0"));
 }
