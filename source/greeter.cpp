299ea41cd9c5145abc1bb18b8e37b7112a9fcb89
Wai-Shing Luk <luk036@gmail.com>
2023-04-21 11:37:42 +0800

    apply fix-format; add xmake.lua; update cxxopts

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -1,8 +1,8 @@
 #include <bingo/greeter.h>
-#include <fmt/format.h>  // for format
+#include <fmt/format.h> // for format
 
 // #include <__config>     // for std
-#include <type_traits>  // for move
+#include <type_traits> // for move
 
 using namespace bingo;
 using namespace std;
@@ -10,15 +10,15 @@ using namespace std;
 Bingo::Bingo(string _name) : name(move(_name)) {}
 
 std::string Bingo::greet(LanguageCode lang) const {
-    switch (lang) {
-        default:
-        case LanguageCode::EN:
-            return fmt::format("Hello, {}!", name);
-        case LanguageCode::DE:
-            return fmt::format("Hallo {}!", name);
-        case LanguageCode::ES:
-            return fmt::format("¡Hola {}!", name);
-        case LanguageCode::FR:
-            return fmt::format("Bonjour {}!", name);
-    }
+  switch (lang) {
+  default:
+  case LanguageCode::EN:
+    return fmt::format("Hello, {}!", name);
+  case LanguageCode::DE:
+    return fmt::format("Hallo {}!", name);
+  case LanguageCode::ES:
+    return fmt::format("¡Hola {}!", name);
+  case LanguageCode::FR:
+    return fmt::format("Bonjour {}!", name);
+  }
 }
